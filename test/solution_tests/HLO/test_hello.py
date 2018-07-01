c6cbb707a3c48fccc3fdcdb741a9ae30cdb6c404
sbx_user1061 <sbx_user1061@ip-10-12-229-13.eu-west-2.compute.internal>
2018-07-01 16:25:39 +0000

    Sun Jul 01 16:25:39 UTC 2018

--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -5,7 +5,7 @@ from solutions.HLO import hello_solution
 
 class TestSum(unittest.TestCase):
     def test_sum(self):
-        self.assertEqual(hello_solution.hello('xxx'), 'Hello, World!')
+        self.assertEqual(hello_solution.hello('xxx'), 'Hello, xxx!')
 
 
 if __name__ == '__main__':
