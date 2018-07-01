35f782516eed424e545fd49b3c613c1ed2f791cf
sbx_user1061 <sbx_user1061@ip-10-12-229-13.eu-west-2.compute.internal>
2018-07-01 16:34:42 +0000

    Sun Jul 01 16:34:42 UTC 2018

--- test/solution_tests/CHK_R1/test_checkout_1.py
+++ test/solution_tests/CHK_R1/test_checkout_1.py
@@ -5,7 +5,7 @@ from solutions.CHK import checkout_solution
 
 class TestCheckout(unittest.TestCase):
     def test_single_sku(self):
-        self.assertEqual(checkout_solution.hello('AABDD'), )
+        self.assertEqual(checkout_solution.hello('AABDD'), 160)
 
 
 if __name__ == '__main__':
