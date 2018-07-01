e1a7f6868e39d0329d80f5aacbd446a7fb5897b6
sbx_user1061 <sbx_user1061@ip-10-12-229-13.eu-west-2.compute.internal>
2018-07-01 16:37:42 +0000

    Sun Jul 01 16:37:42 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,8 +1,27 @@
 
-def get 
+'''
++------+-------+----------------+
+| Item | Price | Special offers |
++------+-------+----------------+
+| A    | 50    | 3A for 130     |
+| B    | 30    | 2B for 45      |
+| C    | 20    |                |
+| D    | 15    |                |
++------+-------+----------------+
+'''
+
+def get_skus():
+    return {
+        'A': {'price':50, 'offers': [{'amount': 3, 'price': 130}]},
+        'B': {'price':30, 'offers': [{'amount': 2, 'price': 45}]},
+        'C': {'price':20, 'offers': []},
+        'D': {'price':15, 'offers': []}
+    }
+
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    for sku in skus:
-        print sku
+    for (sku, options) in get_skus().iteritems():
+
+
